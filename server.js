1b178ae88a2ce93d88c1e18879dfe6f6e0f3f5b4
Shravan Mahajan <shravanm@lambdatest.com>
2024-01-22 14:13:04 +0530

    Add mock api for post method

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const fs = require("fs");
+const https = require("https");
+
 const express = require('express')
 const app = express()
 const port = 3003
+
+
+const key = fs.readFileSync("localhost-key.pem", "utf-8");
+const cert = fs.readFileSync("localhost.pem", "utf-8");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +17,10 @@ app.get('/check', (req, res) => {
   res.status(401).send('Unauthorized')
 })
 
-app.listen(port, () => {
+app.post('/mockfail', (req, res) => {
+  res.status(401).send('Unauthorized')
+})
+
+https.createServer({ key, cert }, app).listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
